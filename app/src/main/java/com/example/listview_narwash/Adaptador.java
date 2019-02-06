a3bf9d6bca28c7caf95976aa71d1a75a2e05516d
Antonio <antomanchecortes1997@gmail.com>
2019-02-06 16:37:40 +0100

    Initial commit

---
+++ app/src/main/java/com/example/listview_narwash/Adaptador.java
@@ -0,0 +1,70 @@
+package com.example.listview_narwash;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class Adaptador extends BaseAdapter {
+
+    private static LayoutInflater inflater = null;
+
+    Context  contexto;
+
+    String [] [] datos;
+    int[] datosImg;
+
+    public Adaptador(Context contexto, String [] [] datos, int [] imagenes){
+        this.contexto = contexto;
+        this.datos = datos;
+        this.datosImg = imagenes;
+        inflater = (LayoutInflater)contexto.getSystemService(contexto.LAYOUT_INFLATER_SERVICE);
+    }
+
+    @Override
+    public View getView(int i, View convertView, ViewGroup parent) {
+
+        final View vista = inflater.inflate(R.layout.elemento_lista,null);
+
+        TextView titulo = (TextView) vista.findViewById(R.id.tvTitulo);
+       // TextView descripcion = (TextView) vista.findViewById(R.id.tvdur);
+        ImageView imagen = (ImageView) vista.findViewById(R.id.imagecapilla);
+        titulo.setText(datos [i] [0]);
+        //descripcion.setText(datos [i] [1]);
+        imagen.setImageResource(datosImg[i]);
+
+        imagen.setTag(i);
+        imagen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent visorImagen = new Intent(contexto, VisorImagen.class);
+                visorImagen.putExtra("IMG", datosImg[(Integer)v.getTag()]);
+                contexto.startActivity(visorImagen);
+            }
+        });
+
+
+        return vista;
+    }
+
+    @Override
+    public int getCount() {
+        return datosImg.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+
+}
---
+++ app/src/main/java/com/example/listview_narwash/VisorImagen.java
@@ -0,0 +1,27 @@
+package com.example.listview_narwash;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class VisorImagen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_visor_imagen);
+
+        ImageView img = (ImageView) findViewById(R.id.imCompleta);
+
+        Intent intent = getIntent();
+        Bundle b = intent.getExtras();
+
+        if(b!=null){
+            img.setImageResource(b.getInt("IMG"));
+        }
+
+
+
+    }
+}
