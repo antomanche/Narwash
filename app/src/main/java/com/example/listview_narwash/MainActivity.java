1d5d7406805f1f3ca2bf66a68495289ad4d35fdd
Cacikon <aagudoarias@gmail.com>
2019-02-08 19:06:49 +0100

    prueba

--- app/src/main/java/com/example/listview_narwash/MainActivity.java
+++ app/src/main/java/com/example/listview_narwash/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.listview_narwash;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ListView;
@@ -27,5 +29,20 @@ public class MainActivity extends AppCompatActivity {
         lista = (ListView) findViewById(R.id.milista);
 
         lista.setAdapter(new Adaptador(this,datos,datosImg));
+     //   openApp(true);
     }
+
+    private void openApp(boolean locationPermission) {
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity
+                        .this, Login.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 2000);
+    }
+
 }
